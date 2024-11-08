5924e9a26f5e706f6b76f77ccc87a22ebe47de45
musekwa <musekwa2011@gmail.com>
2024-11-08 22:48:49 +0200

    update on privacy policy and terms of service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen">
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-4">
       <Image
         src="/logo.png"
         alt="Background Logo"
@@ -21,20 +21,25 @@ export default function Home() {
       />
       <Card className="w-full max-w-md bg-background/90 backdrop-blur-sm">
         <CardHeader className="space-y-2 flex flex-col items-center justify-center">
-          <Image src="/logo.png" alt="Logo" width={80} height={80} />
+          <Image 
+            src="/logo.png" 
+            alt="Logo" 
+            width={80} 
+            height={80}
+            className="w-[60px] md:w-[80px] h-auto" 
+          />
           <div>
-            <CardTitle className="text-center">
+            <CardTitle className="text-center text-xl md:text-2xl">
               Instituto de Amêndoas de Moçambique, IP
             </CardTitle>
           </div>
         </CardHeader>
-        <CardDescription></CardDescription>
         <CardContent>
-          <div className="space-y-4 text-center ">
-            <p className="text-center text-sm text-muted-foreground">
+          <div className="space-y-4 text-center px-2 md:px-4">
+            <p className="text-center text-xs md:text-sm text-muted-foreground">
               Este site ainda está em desenvolvimento.
             </p>
-            <p className="text-center text-sm text-muted-foreground">
+            <p className="text-center text-xs md:text-sm text-muted-foreground">
               Para saber mais sobre o Instituto de Amêndoas de Moçambique, IP e
               o projecto Connect Caju, clique no botão abaixo.
             </p>
